5842e1b3c0fafd086ba5ab13e4a247aa0ba74174
GabrielMarcellino <54562676+GabrielMarcellino@users.noreply.github.com>
2022-06-12 16:23:46 -0300

    Added comments to ESP32.
    Comments on ESP32 and bug fixing in stm32. The bugs appeared after stm32 modularization.

--- ESP32WiFi/ESP32WiFi.ino
+++ ESP32WiFi/ESP32WiFi.ino
@@ -1,16 +1,22 @@
+// USed liraries
 #include "driver/uart.h"
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 
+// Define Buffer
 #define BUF_SIZE (1024)
 
+// Memory handling
 uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
 
+// Wi-fi name and password
 const char* ssid = "Galaxy A719BC3";
 const char* password =  "ogoz7599";
 
+// Declare web server instance on port 80
 AsyncWebServer server(80);
 
+// Uart configuration structure
 uart_config_t uart_config = {
   .baud_rate = 9600,
   .data_bits = UART_DATA_8_BITS,
@@ -20,32 +26,33 @@ uart_config_t uart_config = {
 };
 
 void setup() {
+  // Configure uart peripherals
   uart_param_config(UART_NUM_0, &uart_config);
   uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
   uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
-  
+
+  // Connect to wi-fi
   WiFi.begin(ssid, password);
- 
+
+  // Wait for wi-fi connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
   }
- 
+
+  // Web server execution program waiting for GET HTTP request
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      // Get the parameters from the URL
       AsyncWebParameter* p = request->getParam(0);
+      // Send the received parameters to the stm32 via uart
       uart_write_bytes(UART_NUM_0, (const char *) p->value().c_str(), 55);
+      // Send response to request
       request->send(200, "text/plain", "Configuration string received");
   });
 
-    uart_write_bytes(UART_NUM_0, (const char *) "Y-0500-0125-H@O-0500-0250-H@R-0500-0375-H@B-0500-0000-H", 55);
-    delay(3000);
-    uart_write_bytes(UART_NUM_0, (const char *) "Y-0500-0125-S@O-0500-0250-S@R-0500-0375-S@B-0500-0000-S", 55);
- 
+  // Initiates server
   server.begin();
 }
 
+// Does nothing. Just prevents end of program whilw server is running.
 void loop(){
-    /*uart_write_bytes(UART_NUM_0, (const char *) "Y-0500-0125-H+O-0500-0250-H+R-0500-0375-H+B-0500-0000-H", 55);
-    delay(3000);
-    uart_write_bytes(UART_NUM_0, (const char *) "Y-0500-0125-S+O-0500-0250-S+R-0500-0375-S+B-0500-0000-S", 55);
-    delay(3000);*/
 }
